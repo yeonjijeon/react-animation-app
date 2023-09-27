3029dd4b42380b6ca8b19a47f24c1d84d2ffe66a
전연지 <jeonyeonji1028@gmail.com>
2023-09-27 14:55:58 +0900

    variants (2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { createGlobalStyle } from 'styled-components'
-import { motion } from 'framer-motion'
+import { motion, Variants } from 'framer-motion'
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -79,14 +79,35 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`
+const Circle = styled(motion.div)`
   background-color: white;
-  border-radius: 15px;
+  height: 70px;
+  width: 70px;
+  place-self: center;
+  border-radius: 35px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `
 
-const myVars = {
-  start: { scale: 0 },
-  end: { scale: 1, rotateZ: 360, transition: { type: 'spring', delay: 0.5 } },
+const boxVariants: Variants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      duration: 0.5,
+      bounce: 0.5,
+    },
+  },
 }
 
 function App() {
@@ -94,7 +115,12 @@ function App() {
     <>
       <GlobalStyle />
       <Wrapper>
-        <Box variants={myVars} initial="start" animate="end" />
+        <Box variants={boxVariants} initial="start" animate="end">
+          <Circle />
+          <Circle />
+          <Circle />
+          <Circle />
+        </Box>
       </Wrapper>
     </>
   )
