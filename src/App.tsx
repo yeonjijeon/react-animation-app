0a5a913ec06012b2ae563e72939b63ee677bdbba
YeonJi <jeonyeonji1028@gmail.com>
2023-09-28 20:49:17 +0900

    Gestures

--- src/App.tsx
+++ src/App.tsx
@@ -79,48 +79,13 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
   border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`
-const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  place-self: center;
-  border-radius: 35px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  background-color: rgba(255, 255, 255, 1);
 `
 
 const boxVariants: Variants = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
-    scale: 1,
-    opacity: 1,
-    transition: {
-      type: 'spring',
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.1,
-    },
-  },
-}
-
-const circleVariants: Variants = {
-  start: {
-    opacity: 0,
-    y: 10, // 아래에서 올라옴
-  },
-  end: {
-    opacity: 1,
-    y: 0,
-  },
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: '100px' },
 }
 
 function App() {
@@ -128,12 +93,11 @@ function App() {
     <>
       <GlobalStyle />
       <Wrapper>
-        <Box variants={boxVariants} initial="start" animate="end">
-          <Circle variants={circleVariants} />
-          <Circle variants={circleVariants} />
-          <Circle variants={circleVariants} />
-          <Circle variants={circleVariants} />
-        </Box>
+        <Box
+          variants={boxVariants}
+          whileHover="hover" // 마우스 올렸을 때
+          whileTap="click" // 마우스 클릭했을 때
+        ></Box>
       </Wrapper>
     </>
   )
